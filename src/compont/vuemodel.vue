c45497128e1fcdd51dc9754e312526b93a140e57
杨明月 <yangmingyue@qianshengqian.com>
2018-07-23 16:43:43 +0800

    vue model test

---
+++ src/compont/vuemodel.vue
@@ -0,0 +1,19 @@
+<template>
+<div>v-model:测试</div>
+</template>
+<script>
+export default {
+  model: {
+
+  },
+  props: {
+    title: {
+      type: String,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/module/vueModel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>v-model</h1>
+  </div>
+</template>
+
+<script>
+import directive from "../compont/vuemodel";
+
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
